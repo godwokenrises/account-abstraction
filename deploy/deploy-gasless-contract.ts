8f26339d5668fc23e5766009ee203f71b649dff5
RetricSu <inneverland2013@gmail.com>
2022-12-28 15:07:21 +0800

    fix: lint

--- deploy/deploy-gasless-contract.ts
+++ deploy/deploy-gasless-contract.ts
@@ -9,9 +9,9 @@ export const deployEntryPoint = async function (gwFullNodeAddress: string, payma
   const paymasterStake: BigNumber = paymasterStakeInEth ? ethers.utils.parseEther(paymasterStakeInEth) : DEFAULT_PAYMASTER_STAKE
   const unstackedDelSec: number = unstackedDelaySec ?? DEFAULT_UNSTACKED_DELAY_SEC
 
-  const Contract = await ethers.getContractFactory("GaslessEntryPoint")
-  const contract = await Contract.connect(admin).deploy(gwFullNodeAddress, paymasterStake, unstackedDelSec);
-  await contract.deployed();
+  const Contract = await ethers.getContractFactory('GaslessEntryPoint')
+  const contract = await Contract.connect(admin).deploy(gwFullNodeAddress, paymasterStake, unstackedDelSec)
+  await contract.deployed()
 
   console.log('==Entrypoint addr=', contract.address)
 }
@@ -19,48 +19,46 @@ export const deployEntryPoint = async function (gwFullNodeAddress: string, payma
 export const deployPaymaster = async function (entrypointAddress: string) {
   const [admin] = await ethers.getSigners()
 
-  const Contract = await ethers.getContractFactory("GaslessDemoPaymaster")
-  const contract = await Contract.connect(admin).deploy(entrypointAddress);
-  await contract.deployed();
+  const Contract = await ethers.getContractFactory('GaslessDemoPaymaster')
+  const contract = await Contract.connect(admin).deploy(entrypointAddress)
+  await contract.deployed()
 
   console.log('==Paymaster addr=', contract.address)
 }
 
 export const addPaymasterStake = async function (paymasterAddr: string, ethAmount: string) {
   const [admin] = await ethers.getSigners()
-  const Contract = await ethers.getContractFactory("GaslessDemoPaymaster")
-  const contract = await Contract.attach(paymasterAddr);
+  const Contract = await ethers.getContractFactory('GaslessDemoPaymaster')
+  const contract = await Contract.attach(paymasterAddr)
   const tx = await contract.connect(admin).addStake(99999999, { value: ethers.utils.parseEther(ethAmount) })
-  console.log('addPaymasterStake tx sent: ', tx.hash);
-  const receipt = await tx.wait();
-  console.log('tx receipt: ', receipt);
+  console.log('addPaymasterStake tx sent: ', tx.hash)
+  const receipt = await tx.wait()
+  console.log('tx receipt: ', receipt)
 }
 
 export const depositPaymasterStake = async function (entrypointAddr: string, paymasterAddr: string, ethAmount: string) {
   const [admin] = await ethers.getSigners()
-  const Contract = await ethers.getContractFactory("GaslessEntryPoint")
-  const contract = await Contract.attach(entrypointAddr);
+  const Contract = await ethers.getContractFactory('GaslessEntryPoint')
+  const contract = await Contract.attach(entrypointAddr)
   const tx = await contract.connect(admin).depositTo(paymasterAddr, { value: ethers.utils.parseEther(ethAmount) })
-  console.log('depositPaymasterStake tx sent: ', tx.hash);
-  const receipt = await tx.wait();
-  console.log('tx receipt: ', receipt);
+  console.log('depositPaymasterStake tx sent: ', tx.hash)
+  const receipt = await tx.wait()
+  console.log('tx receipt: ', receipt)
 }
 
 export const getBalance = async function (entrypointAddr: string, paymasterAddr: string) {
-  const Contract = await ethers.getContractFactory("GaslessEntryPoint")
-  const contract = await Contract.attach(entrypointAddr);
-  const balance = await contract.balanceOf(paymasterAddr);
-  console.log('paymaster balance: ', balance);
+  const Contract = await ethers.getContractFactory('GaslessEntryPoint')
+  const contract = await Contract.attach(entrypointAddr)
+  const balance = await contract.balanceOf(paymasterAddr)
+  console.log('paymaster balance: ', balance)
 }
 
 export const addWhitelistAddress = async function (paymasterAddr: string, whitelistAddress: string) {
   const [admin] = await ethers.getSigners()
-  const Contract = await ethers.getContractFactory("GaslessDemoPaymaster")
-  const contract = await Contract.attach(paymasterAddr);
-  const tx = await contract.connect(admin).addWhitelistAddress(whitelistAddress);
-  console.log('addWhitelistAddress tx sent: ', tx.hash);
-  const receipt = await tx.wait();
-  console.log('tx receipt: ', receipt);
+  const Contract = await ethers.getContractFactory('GaslessDemoPaymaster')
+  const contract = await Contract.attach(paymasterAddr)
+  const tx = await contract.connect(admin).addWhitelistAddress(whitelistAddress)
+  console.log('addWhitelistAddress tx sent: ', tx.hash)
+  const receipt = await tx.wait()
+  console.log('tx receipt: ', receipt)
 }
-
-
--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,38 +1,38 @@
-import hre from "hardhat";
+import hre from 'hardhat'
 import {
-	addPaymasterStake,
+  addPaymasterStake,
   addWhitelistAddress,
   deployPaymaster,
   depositPaymasterStake,
-  getBalance,
-} from "../deploy/deploy-gasless-contract";
+  getBalance
+} from '../deploy/deploy-gasless-contract'
 
-async function main() {
-  //await deployPaymasterContract();
+async function main () {
+  // await deployPaymasterContract();
 
   // await addWhitelistAddress(
   //   "0xa8a0B28AbC58290EF91Dd4375Ea5e1274dE16f47",
   //   "0x73b72d6EE63e16c898AD18C7f447846BfC3AB1aC"
   // );
 
-  //await addPaymasterStake("0xa8a0B28AbC58290EF91Dd4375Ea5e1274dE16f47", "1000");
-  await depositPaymasterStake("0xc52059c8cd8f0817f9e67009e014322f5239547f", "0xa8a0B28AbC58290EF91Dd4375Ea5e1274dE16f47", "1000");
-  await getBalance("0xc52059c8cd8f0817f9e67009e014322f5239547f", "0xa8a0B28AbC58290EF91Dd4375Ea5e1274dE16f47")
+  // await addPaymasterStake("0xa8a0B28AbC58290EF91Dd4375Ea5e1274dE16f47", "1000");
+  await depositPaymasterStake('0xc52059c8cd8f0817f9e67009e014322f5239547f', '0xa8a0B28AbC58290EF91Dd4375Ea5e1274dE16f47', '1000')
+  await getBalance('0xc52059c8cd8f0817f9e67009e014322f5239547f', '0xa8a0B28AbC58290EF91Dd4375Ea5e1274dE16f47')
 }
 
 const deployPaymasterContract = async () => {
-  const entrypoint = process.env.ENTRYPOINT;
+  const entrypoint = process.env.ENTRYPOINT
   if (entrypoint == null) {
-    throw new Error("process.env.ENTRYPOINT is null");
+    throw new Error('process.env.ENTRYPOINT is null')
   }
-  await deployPaymaster(entrypoint);
-};
+  await deployPaymaster(entrypoint)
+}
 
 // We recommend this pattern to be able to use async/await everywhere
 // and properly handle errors.
 main()
   .then(() => process.exit(0))
   .catch((error) => {
-    console.error(error);
-    process.exit(1);
-  });
+    console.error(error)
+    process.exit(1)
+  })
